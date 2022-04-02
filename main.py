a48c6481a3d78fac25e12b586528345663c8755b
stts <it.spectre.ru@gmail.com>
2022-04-03 00:07:44 +0500

    get intent

---
+++ main.py
@@ -0,0 +1,18 @@
+BOT_CONFIG = {
+    'intents': {
+        'hello': {
+            'examples': ['привет', 'прив.'],
+            'responses': ['здравствуйте', 'здоров.']
+        },
+        'bye': {
+            'examples': ['пока', 'прощай', 'до свидания'],
+            'responses': ['увидимся', 'бывай', 'до связи']
+        }
+        
+    }
+}
+
+def get_intent(text):
+    print('deepeeer')
+
+get_intent('dsads')
