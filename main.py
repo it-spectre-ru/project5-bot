ca9956c9a3001fa97dcb20f84a00d2fa3609c8c8
stts <it.spectre.ru@gmail.com>
2022-04-08 10:50:46 +0500

    поиск интента и вывод ответа.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import random
+
+
 BOT_CONFIG = {
     'intents': {
         'hello': {
-            'examples': ['привет', 'прив.'],
+            'examples': ['привет', 'прив.', 'zdorov'],
             'responses': ['здравствуйте', 'здоров.']
         },
         'bye': {
@@ -14,8 +17,21 @@ BOT_CONFIG = {
 
 def get_intent(text):
     for intent in BOT_CONFIG['intents'].keys():
-        print(intent)
         for example in BOT_CONFIG['intents'][intent]['examples']:
-            print(example)
+            if example == text:
+                return intent
+    return 'интент не найден'
 
-get_intent('dsads')
+def bot(input_text):
+  intent = get_intent(input_text)
+  if intent != 'интент не найден':
+    return random.choice(BOT_CONFIG['intents'][intent]['responses'])
+  else:
+    return 'интент не найден'
+
+input_text = ''
+while input_text != 'stop':
+  input_text = input()
+  if input_text != 'stop':
+    response = bot(input_text)
+    print(response) 
