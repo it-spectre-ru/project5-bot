e36b3a9072c01009c1a3a7256bd9466f80c55e21
stts <it.spectre.ru@gmail.com>
2022-04-11 10:21:42 +0500

    расстояние ливенштейна

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import nltk
 
 
 BOT_CONFIG = {
@@ -15,12 +16,23 @@ BOT_CONFIG = {
     }
 }
 
+
+def clean(text):
+  clean_text = ''
+  for ch in text.lower():
+    if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
+      clean_text = clean_text + ch
+  return clean_text
+
+
 def get_intent(text):
-    for intent in BOT_CONFIG['intents'].keys():
-        for example in BOT_CONFIG['intents'][intent]['examples']:
-            if example == text:
-                return intent
-    return 'интент не найден'
+  for intent in BOT_CONFIG['intents'].keys():
+    for example in BOT_CONFIG['intents'][intent]['examples']:
+      s1 = clean(text)
+      s2 = clean(example)
+      if nltk.edit_distance(s1, s2) / max(len(s1), len(s2)) < 0.4:
+        return intent
+  return 'интент не найден'
 
 def bot(input_text):
   intent = get_intent(input_text)
