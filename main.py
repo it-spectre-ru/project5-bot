5c32c5dc9620543bd0d1be2e56566a5b6d09e427
stts <it.spectre.ru@gmail.com>
2022-05-08 15:29:53 +0500

    улучшил определение интента

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+
+
+
+def clean(text):
+  clean_text = ''
+  for ch in text.lower():
+    if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ':
+      clean_text = clean_text + ch
+  return clean_text
 
 
 with open ('BOT_CONFIG.json') as f:
@@ -19,23 +29,28 @@ for intent in BOT_CONFIG['intents'].keys():
     y.append(intent)
 
 
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(texts)
+train_texts, test_texts, y_train, y_test = train_test_split(texts, y, random_state=42, test_size=0.32)
+
+
+vectorizer = CountVectorizer(ngram_range=(1,5))
+X_train = vectorizer.fit_transform(train_texts)
+X_test = vectorizer.transform(test_texts)
 
 
 vocab = vectorizer.get_feature_names_out()
 
 
-clf = LogisticRegression().fit(X, y)
-clf.score(X, y)
+# len(vocab)
 
 
-def clean(text):
-  clean_text = ''
-  for ch in text.lower():
-    if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
-      clean_text = clean_text + ch
-  return clean_text
+clf = LogisticRegression().fit(X_train, y_train)
+clf.score(X_train, y_train), clf.score(X_test, y_test)
+
+
+clf.predict(vectorizer.transform(['Шкаф']))
+
+
+BOT_CONFIG['intents']['zamer']
 
 
 def get_intent_by_model(text):
@@ -53,4 +68,3 @@ while input_text != 'stop':
   if input_text != 'stop':
     response = bot(input_text)
     print(response) 
-  
