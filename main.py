298633dd879b6723eaecd7af86887cd681cdb9c8
stts <it.spectre.ru@gmail.com>
2022-05-08 16:03:19 +0500

    RandomForestClassifier

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 import nltk
 import json
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 
@@ -19,6 +20,8 @@ def clean(text):
 
 with open ('BOT_CONFIG.json') as f:
   BOT_CONFIG = json.load(f)
+# len(BOT_CONFIG['intents'].keys())
+# BOT_CONFIG.keys()
 
 
 texts = []
@@ -32,7 +35,7 @@ for intent in BOT_CONFIG['intents'].keys():
 train_texts, test_texts, y_train, y_test = train_test_split(texts, y, random_state=42, test_size=0.32)
 
 
-vectorizer = CountVectorizer(ngram_range=(1,5), analyzer='char_wb')
+vectorizer = CountVectorizer(ngram_range=(1,3), analyzer='char_wb') #TfidfVectorizer(ngram_range=(1,5), analyzer='char_wb')  
 X_train = vectorizer.fit_transform(train_texts)
 X_test = vectorizer.transform(test_texts)
 
@@ -40,14 +43,15 @@ X_test = vectorizer.transform(test_texts)
 vocab = vectorizer.get_feature_names_out()
 
 
-len(vocab)
+# len(vocab)
+# vocab
 
 
-clf = LogisticRegression().fit(X_train, y_train)
+clf = RandomForestClassifier(n_estimators=300).fit(X_train, y_train)  #LogisticRegression().fit(X_train, y_train) #LogisticRegression(C=2) штрафует модель за зазубривание
 clf.score(X_train, y_train), clf.score(X_test, y_test)
 
 
-clf.predict(vectorizer.transform(['замер']))
+clf.predict(vectorizer.transform(['заказ']))
 
 
 BOT_CONFIG['intents']['zamer']
@@ -60,7 +64,7 @@ def get_intent_by_model(text):
 def bot(input_text):
   intent = get_intent_by_model(input_text)
   return random.choice(BOT_CONFIG['intents'][intent]['responses'])
-
+  
 
 input_text = ''
 while input_text != 'stop':
